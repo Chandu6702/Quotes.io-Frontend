3fe9d2c7d51018de5a269744b6e3ca9685dc035b
Uma Chandra Sekhara Sarma <chandu06p@gmail.com>
2024-03-13 23:19:47 +0530

    update Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,47 +1,48 @@
-import React from 'react'
-import Styles from './Home.module.css'
-import SlideShow from './SlideShow/SlideShow'
+import React from "react";
+import Styles from "./Home.module.css";
+import SlideShow from "./SlideShow/SlideShow";
 function Home() {
   return (
-    <div className={Styles['container']}>
-
-      <section id={Styles['hero']}>
-        <SlideShow/>
+    <div className={Styles["container"]}>
+      <section id={Styles["hero"]}>
+        <SlideShow />
       </section>
 
-      <hr/>
+      <hr />
 
-      <section id={Styles['info-1']}>
-        <ul id={Styles['list']}>
-          <li className={Styles['items']}>
+      <section id={Styles["info-1"]}>
+        <ul id={Styles["list"]}>
+          <li className={Styles["items"]}>
             <h3>blasd sd asd sda sd</h3>
-            <p>
-              ujbsd dsiouh a ashgd  dsadasda sd sdada 
-            </p>
+            <p>ujbsd dsiouh a ashgd dsadasda sd sdada</p>
           </li>
-          <div className={Styles['lines']}></div>
-          <li className={Styles['items']}>
+          <div className={Styles["lines"]}></div>
+          <li className={Styles["items"]}>
             <h3>blasd sd asd sda sd</h3>
-            <p>
-              ujbsd dsiouh a ashgd  dsadasda sd sdada 
-            </p>
+            <p>ujbsd dsiouh a ashgd dsadasda sd sdada</p>
           </li>
-          <div className={Styles['lines']}></div>
-          <li className={Styles['items']}>
+          <div className={Styles["lines"]}></div>
+          <li className={Styles["items"]}>
             <h3>blasd sd asd sda sd</h3>
-            <p>
-              ujbsd dsiouh a ashgd  dsadasda sd sdada 
-            </p>
+            <p>ujbsd dsiouh a ashgd dsadasda sd sdada</p>
           </li>
         </ul>
       </section>
 
-      <hr/>
+      <hr />
 
-      <section id='info-2'></section>
+      <section id={Styles["info-2"]}>
+        <div className={Styles["img-card"]}>
+          <h2>Image</h2>
+        </div>
+        {/* <div className={Styles["lines"]}></div> */}
 
+        <div className={Styles["para"]}>
+          <h2>fjfflkkkfklf.fkfko'k;kf;kfkfkf;k,gjfsflw</h2>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
