a340baa32b62833095fed73a4f2ac90625c59f94
Uma Chandra Sekhara Sarma <chandu06p@gmail.com>
2024-04-29 15:40:43 +0530

    quotes update

--- src/components/Quotes/Filter/Filter.jsx
+++ src/components/Quotes/Filter/Filter.jsx
@@ -3,10 +3,10 @@ import Styles from "./Filter.module.css";
 
 function Filter() {
   return (
-    <div id={Styles['container']}>
-      <section id={Styles['genere']}>
-        <h2>Genere</h2>
-        <hr/>
+    <div id={Styles["container"]}>
+      <section id={Styles["genres"]}>
+        <h3>Genres</h3>
+        <hr />
         <ul>
           <li>motivational</li>
           <li>love</li>
@@ -15,14 +15,20 @@ function Filter() {
           <li>happy</li>
         </ul>
       </section>
-      <section id={Styles['sort-by']}>
-        <h2>Sort-By</h2>
-        <hr/>
-        <label for="latest"><input type="radio" id={Styles['latest']}/> Latest</label>
-        
-        <label for="oldest"><input type="radio" id={Styles['oldest']}/> oldest</label>
-        
-        <label for="most-liked"><input type="radio" id={Styles['most-liked']}/> Most-liked</label>
+      <section id={Styles["sort-by"]}>
+        <h3>Sort-By</h3>
+        <hr />
+        <label for="latest">
+          <input type="radio" id={Styles["latest"]} name="choice" /> Latest
+        </label>
+
+        <label for="oldest">
+          <input type="radio" id={Styles["oldest"]} name="choice"/> oldest
+        </label>
+
+        <label for="most-liked">
+          <input type="radio" id={Styles["most-liked"]} name="choice"/> Most-liked
+        </label>
       </section>
     </div>
   );
--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -7,7 +7,7 @@ function Quotes() {
   return (
     <div id={Styles['container']}>
       <div id={Styles['search-bar']}>
-        <input type='search' placeholder='Type here to search'/>
+        <input type='search' placeholder='🔎Type here to search'/>
         <button type='submit'>Search</button>
       </div>
       <div id={Styles['section']}>
