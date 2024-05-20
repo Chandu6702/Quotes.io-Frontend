4cbb726600f3f3e59a74bb763f22b43da2fe80cd
Uma Chandra Sekhara Sarma Thadepalli <chandrasekhara067@gmail.com>
2024-05-20 20:30:10 +0530

    add icons

--- src/components/Quotes/Card/Card.jsx
+++ src/components/Quotes/Card/Card.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import Styles from './Card.module.css'
+import React, { useState } from "react";
+import Styles from "./Card.module.css";
+import Heart from "react-animated-heart";
+import { FaRegComment } from "react-icons/fa6";
+import { IoIosShareAlt } from "react-icons/io";
+import { FcLike } from "react-icons/fc";
 
-function Card({text}) {
-return (
-    <div className={Styles['container']}>
-        {text}
+function Card({ text }) {
+  const [isClick, setClick] = useState(false);
+  return (
+    <div className={Styles["container"]}>
+      <p className={Styles["quote"]}>{text}</p>
+      <div className={Styles["wrapper"]}>
+        <div className={Styles["icons"]}>
+          <div className={Styles['like']}>
+            <FcLike size={30} color="rgba(255, 255, 255, 0.581)" />
+            143
+          </div>
+          <div className={Styles['comment']}>
+            <FaRegComment size={30} color="rgba(255, 255, 255, 0.581)" />
+            143
+          </div>
+          <div className={Styles['share']}>
+            <IoIosShareAlt size={30} color="rgba(255, 255, 255, 0.581)" />
+          </div>
+        </div>
+        <div>
+          <p className={Styles["author"]}>@monkeydfgfdfgdfdluffy</p>
+        </div>
+      </div>
     </div>
-)
+  );
 }
 
-export default Card
+export default Card;
--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import Styles from './Quotes.module.css'
-import Card from './Card/Card.jsx'
-import Filter from './Filter/Filter.jsx'
+import React from 'react';
+import Styles from './Quotes.module.css';
+import Card from './Card/Card.jsx';
+import Filter from './Filter/Filter.jsx';
+import { FiSearch } from "react-icons/fi";
+import { useSearchParams } from 'react-router-dom';
+
 
 function Quotes() {
+
+  const [searchParam,setSearchParam]=useSearchParams();
+
+  const handleChange=(e)=>{
+    setSearchParam((prev)=>{
+        prev.set("search",e.target.value);
+        return prev;
+      });
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(searchParam.get("search"));
+  }
+
   return (
     <div id={Styles['container']}>
+
       <div id={Styles['search-bar']}>
-        <input type='search' placeholder='🔎Type here to search'/>
-        <button type='submit'>Search</button>
+        <label className={Styles['wrapper']}> 
+        <FiSearch color='grey' size="25"/>
+        <input type='search' placeholder='Type here to search' value={searchParam.get("search")} onChange={handleChange} maxLength="50"/>
+        </label>
+        <button type='submit' onClick={handleSubmit}>Search</button>
       </div>
+
       <div id={Styles['section']}>
         <div id={Styles['filter']}>
             <Filter/>
         </div>
         <div id={Styles['cards']}>
-            <Card text="hgghuybghhgbbhhnbbbbb"/>
+            <Card text="hgghuybghhgbbhhnbbbbbffsdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"/>
             <Card text="khjhmvbcj jhgkvjhg.b.lkhkhkhk"/>
             <Card text="hgghuybghhgbbhhjvjmbjhkh,n,j"/>
             <Card text="hgghuybghnvjbvhn jbmmb"/>
